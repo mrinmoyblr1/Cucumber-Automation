3f91dcaa6b8e9ce16cbe783352944246d7770309
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro.local>
2025-06-17 17:31:15 -0400

    Committing New Files v1

--- src/test/java/stepDefinitions/LandingPageStepDefinition.java
+++ src/test/java/stepDefinitions/LandingPageStepDefinition.java
@@ -1,42 +1,37 @@
 package stepDefinitions;
-
-
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-
-import java.util.Iterator;
-import java.util.Set;
+import utils.TestContextSetup;
 
 public class LandingPageStepDefinition {
-    public WebDriver driver;
-    public String landingPageproductName;
-    public String offerPageProductName;
+    //public WebDriver driver;
+    TestContextSetup testContextSetup;
+
+
+    public LandingPageStepDefinition(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+
+    }
 
     @Given("^User is on GreenCart landing Page$")
     public void user_is_on_greencart_landing_page() throws Throwable {
         System.setProperty("webdriver.chome.driver", "/Users/mrinmoy/Documents/Development/Selenium/chromedriver");
-        driver = new FirefoxDriver();
+        testContextSetup.driver = new FirefoxDriver();
         //driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        testContextSetup.driver.manage().window().maximize();
+        testContextSetup.driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         //driver.close();
     }
 
     @When("User searched with shortname {string} and extracted the actual name of the Product")
     public void userSearchedWithShortnameAndExtractedTheActualNameOfTheProduct(String shortName) throws Throwable {
 
-        driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
+        testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
         Thread.sleep(5000);
-        landingPageproductName = driver.findElement(By.cssSelector("h4[class='product-name']")).getText().split("-")[0].trim();
-        System.out.println("The Product Name is extracted from Home Page: " + landingPageproductName);
+        testContextSetup.landingPageProductName = testContextSetup.driver.findElement(By.cssSelector("h4[class='product-name']")).getText().split("-")[0].trim();
+        System.out.println("The Product Name is extracted from Home Page: " + testContextSetup.landingPageProductName);
 
     }
-
-
 }
