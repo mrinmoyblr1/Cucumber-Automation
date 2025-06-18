1564a91115a98a3762d9fa75b0881e9ba5fff3d8
mrinmoy.blr@gmail.com <mrinmoy.blr2@gmail.com>
2025-06-18 13:09:42 -0400

    Commit on 06-18-2025-001

--- src/test/java/features/searchProduct.feature
+++ src/test/java/features/searchProduct.feature
@@ -5,7 +5,7 @@ Feature: Search and Place the Order for Products
     Given User is on GreenCart landing Page
     When User searched with shortname "Tom" and extracted the actual name of the Product
     Then User searched for "Tom" shortname in the offer page
-    And validate product name in offers page matches with Landing Page
+#    And validate product name in offers page matches with Landing Page
 #    Examples:
 #      | Name  |
 #      | Tom   |
--- src/test/java/stepDefinitions/OfferPageStepDefinition.java
+++ src/test/java/stepDefinitions/OfferPageStepDefinition.java
@@ -1,16 +1,15 @@
 package stepDefinitions;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.testng.Assert;
 import pageObjects.LandingPage;
 import pageObjects.OffersPage;
 import utils.TestContextSetup;
+
 public class OfferPageStepDefinition {
     public String offerPageProductName;
     TestContextSetup testContextSetup;
-
-
-
 
 
     public OfferPageStepDefinition(TestContextSetup testContextSetup) {
@@ -18,35 +17,41 @@ public class OfferPageStepDefinition {
     }
 
 
-
-
     @Then("User searched for {string} shortname in the offer page")
     public void user_searched_for_same_shortname_in_the_offer_page_to_check_if_product_is_exist(String shortName) throws Throwable {
+
         switchToOffersPage();
 
+        System.out.println("Check point inside ====1111====");
         OffersPage offersPage = new OffersPage(testContextSetup.driver);
+        System.out.println("Check point inside ====2222====");
 
         offersPage.searchItem(shortName);
+        System.out.println("Check point inside ====3333====");
         offerPageProductName = offersPage.getProductName();
+        System.out.println("Check point inside ====4444====");
         System.out.println("The Product Name is extracted from Offer Page: " + offerPageProductName);
         Thread.sleep(5000);
     }
 
 
-
-
     public void switchToOffersPage() throws Throwable {
+
+
         // If  already switched to Offer Page -> Skip below Part
         //if(testContextSetup.driver.getCurrentUrl().equalsIgnoreCase("https://rahulshettyacademy.com/seleniumPractise/#/offers"))
 
         LandingPage landingPage = testContextSetup.pageObjectManager.getLandingPage();  // Here we are eliminating object creation
         landingPage.selectTopDealsPage();
+
         testContextSetup.genericUtils.SwitchWindowToChild();
+
 
     }
 
     @And("^validate product name in offers page matches with Landing Page$")
     public void validate_product_name_in_offers_page_matches_with_landing_page() throws Throwable {
+        System.out.println("Check point inside ========");
         Assert.assertEquals(offerPageProductName, testContextSetup.landingPageProductName);
     }
 }
