c340a91e2777764ea366e517ed7558364acf0e08
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro.local>
2025-06-19 07:41:07 -0400

    On 06-19-2025 001

--- src/test/java/stepDefinitions/OfferPageStepDefinition.java
+++ src/test/java/stepDefinitions/OfferPageStepDefinition.java
@@ -46,7 +46,6 @@ public class OfferPageStepDefinition {
 
     @And("^validate product name in offers page matches with Landing Page$")
     public void validate_product_name_in_offers_page_matches_with_landing_page() throws Throwable {
-        System.out.println("Check point inside ========");
         Assert.assertEquals(offerPageProductName, testContextSetup.landingPageProductName);
     }
 }
--- src/test/java/utils/TestContextSetup.java
+++ src/test/java/utils/TestContextSetup.java
@@ -8,14 +8,14 @@ public class TestContextSetup {
     public WebDriver driver;
     public String landingPageProductName;
     public PageObjectManager pageObjectManager;
-    public  TestBase testBase;
-    public  GenericUtils genericUtils;
+    public TestBase testBase;
+    public GenericUtils genericUtils;
 
 
     public TestContextSetup() {
-        testBase=new TestBase();
+        testBase = new TestBase();
         pageObjectManager = new PageObjectManager(testBase.WebDriverManager());
-        genericUtils=new GenericUtils(testBase.WebDriverManager());
+        genericUtils = new GenericUtils(testBase.WebDriverManager());
 
 
     }
