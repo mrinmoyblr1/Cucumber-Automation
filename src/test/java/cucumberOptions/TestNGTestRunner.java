8d943c7eab2b048e78178342d80cb5eaeb564d88
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro.local>
2025-06-22 23:57:41 -0400

    On 06-22-2025 001

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -8,12 +8,14 @@ import org.testng.annotations.DataProvider;
         features = "src/test/java/features"
         //, dryRun = true   // dryRun = true. This is just like compile. It will check if all the lines under features file has respected step definition or not !
         , glue = "stepDefinitions"
-        , tags = "@PlaceOrder"
+        , tags = "@PlaceOrder or @OffersPage"
         //, tags = "@SmokeTest or @RegressionTest"
         //, tags = "@SmokeTest and @RegressionTest"
         // , tags = "not @RegressionTest"
         //,monochrome = true  // This will make the test output more readable, without it there will be some alphanumeric values
-        , plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cukes.xml"}
+        , plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cukes.xml",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
     @Override
