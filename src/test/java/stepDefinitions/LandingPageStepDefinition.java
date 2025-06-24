49b37ac5b4df245f568ad1f106b6be38c6440dfd
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro.local>
2025-06-24 11:41:24 -0400

    06-24-2025 001

--- src/test/java/stepDefinitions/LandingPageStepDefinition.java
+++ src/test/java/stepDefinitions/LandingPageStepDefinition.java
@@ -18,9 +18,8 @@ public class LandingPageStepDefinition {
     @Given("^User is on GreenCart landing Page$")
     public void user_is_on_greencart_landing_page() throws Throwable {
         Assert.assertTrue(landingPage.getTitleLandingPage().contains("GreenKart"));
-        System.out.println("Browser opened successfully...");
-        System.out.println("Browser opened successfully...");
-        System.out.println("Browser opened successfully...");
+        System.out.println("Browser opened successfully.........");
+
     }
 
     @When("^User searched with shortname (.+) and extracted the actual name of the Product$")
