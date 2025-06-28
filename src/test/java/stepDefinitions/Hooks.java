cf4131833bcf6f2afee3a7013c6860042205e0ab
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro.local>
2025-06-27 23:24:27 -0400

    From MAC Pro 06-27-2025 002

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -7,7 +7,6 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
 import utils.TestContextSetup;
 
 import java.io.File;
